edc6b5a47fa76405db4df5e5bf9c8be7f1ddb17c
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-06-25 21:54:41 +0200

    feat: add graphql support for the custom product-size service

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,42 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin('graphql').service('extension');
+
+    extensionService.use(({ nexus }) => {
+      const updateProductSize = nexus.extendType({
+        type: 'Mutation',
+        definition(t) {
+          // "update" query definition
+          t.field('updateProductSize', {
+            // Response type
+            type: nexus.nonNull('ComponentProductsSizes'),
+
+            // Args definition
+            args: { id: nexus.nonNull('ID'), data: nexus.nonNull('ComponentProductsSizesInput') },
+
+            // Resolver definition
+            resolve(parent, args, context) {
+              const { id, data } = args;
+              return strapi.service('api::product.product-size').update(id, data);
+            }
+          });
+        }
+      });
+
+      return {
+        types: [updateProductSize],
+        resolversConfig: {
+          'Mutation.updateProductSize': {
+            auth: {
+              scope: ['api::product.product-size.update']
+            }
+          }
+        }
+      };
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before

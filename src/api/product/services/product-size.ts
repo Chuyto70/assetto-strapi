9c9e28ef90b19a473b3ba74d7888e8fbb1f3c69e
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-06-25 23:27:47 +0200

    feat: add logic for the product-size quantity

--- src/api/product/services/product-size.ts
+++ src/api/product/services/product-size.ts
@@ -11,13 +11,26 @@ const { ApplicationError } = utils.errors;
 export default factories.createCoreService('api::product.product', ({ strapi }) => ({
     //Creating the update custom service
   update: async (sizeId, data = {}) => {
-      try {
-        return await strapi.query('products.sizes').update({
-          where: { id: sizeId },
-          data: data
-        });
-      } catch (error) {
-        throw new ApplicationError('Something went wrong while updating product-size service', { error: error });
+    try {
+      const size = await strapi.query('products.sizes').findOne({
+        where: { id: sizeId },
+        populate: {
+          quantity: true
+        }
+      });
+      if (data.hasOwnProperty('quantity')) {
+        const dataQty = data['quantity'];
+        const isPositive = dataQty >= 0;
+        if ((size.quantity - (-dataQty)) < 0) throw new ApplicationError('You cannot go under 0 for the quantity');
+        data['quantity'] = isPositive ? size.quantity + dataQty : size.quantity - (-dataQty);
       }
-    },
+      
+      return await strapi.query('products.sizes').update({
+        where: { id: sizeId },
+        data: data
+      });
+    } catch (error) {
+      throw new ApplicationError('Something went wrong while updating product-size service', { error: error });
+    }
+  },
 }));

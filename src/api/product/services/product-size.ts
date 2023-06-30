fa7bdaaa79e55993753d22f041aaefdc8d9fe312
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-06-30 19:33:47 +0200

    fix: update function on product-size service was not able to handle concurrent updates

--- src/api/product/services/product-size.ts
+++ src/api/product/services/product-size.ts
@@ -11,24 +11,31 @@ const { ApplicationError } = utils.errors;
 export default factories.createCoreService('api::product.product', ({ strapi }) => ({
     //Creating the update custom service
   update: async (sizeId, data = {}) => {
+
+    const knex = strapi.db.connection;
+    const transaction = await knex.transaction();
+
     try {
-      const size = await strapi.query('products.sizes').findOne({
-        where: { id: sizeId }
-      });
-      if (data.hasOwnProperty('quantity')) {
-        const dataQty = data['quantity'];
-        if (size.quantity === -1) throw new ApplicationError('You cannot update an infinite quantity (update from the backend directly)');
-        if ((size.quantity + dataQty) < 0) throw new ApplicationError('You cannot go under 0 for the quantity');
-        data['quantity'] = size.quantity + dataQty;
-      }
+      const updateQuery = transaction('components_products_sizes').where('id', sizeId);
       
-      return await strapi.query('products.sizes').update({
+      if (data.hasOwnProperty('quantity')) {
+        const { quantity } = await transaction('components_products_sizes').select('quantity').where({ id: sizeId }).first();
+        const dataQty = data['quantity'];
+        updateQuery.andWhere('quantity', '>', -1);
+        if ((quantity + dataQty) < 0) throw new ApplicationError('You cannot go under 0 for the quantity');
+        if (dataQty < 0) updateQuery.andWhere('quantity', '>=', dataQty);
+        data['quantity'] = quantity + dataQty;
+      }
+
+      await updateQuery.update(data);
+      await transaction.commit();
+      return await strapi.query('products.sizes').findOne({
         where: { id: sizeId },
-        data: data,
         select: Object.keys(data)
       });
     } catch (error) {
-      throw new ApplicationError('Something went wrong while updating product-size service', { error: error });
+      await transaction.rollback();
+      throw new ApplicationError('Something went wrong while updating product-size service', { error });
     }
   },
 }));
